950d9ec7f2419211e6b10a721cbb625198825449
eracah <ejracah@gmail.com>
2019-10-19 16:21:27 -0400

    removed postprocessing from inside the sklearn probe

--- scripts/run_probe.py
+++ scripts/run_probe.py
@@ -7,6 +7,7 @@ import wandb
 import sys
 from src.majority import majority_baseline
 from atariari.benchmark.episodes import get_episodes
+from atariari.benchmark.probe import postprocess_raw_metrics
 import pandas as pd
 import numpy as np
 from copy import deepcopy
@@ -71,10 +72,10 @@ def run_probe(args):
                                       patience=args.patience)
 
         cat_test_acc, cat_test_f1 = trainer.train_test(f_tr, y_tr, f_test, y_test)
-
+        cat_test_acc, cat_test_f1 = postprocess_raw_metrics(cat_test_acc, cat_test_f1)
         # cat_test_acc = prepend_prefix(cat_test_acc, "all_slots_")
         # wandb.run.summary.update(cat_test_acc)
-        cat_test_f1 = prepend_prefix(cat_test_f1, "all_slots_")
+        cat_test_f1 = append_suffix(cat_test_f1, "_all_slots")
         wandb.run.summary.update(cat_test_f1)
 
 
@@ -94,32 +95,36 @@ def run_probe(args):
             accs.append(deepcopy(test_acc))
             f1s.append(deepcopy(test_f1score))
             # sloti_test_acc = prepend_prefix(test_acc, "slot{}_".format(i+1))
-            #sloti_test_f1 = prepend_prefix(test_f1score, "slot{}_".format(i+1))
+            sloti_test_f1 = append_suffix(test_f1score, "_f1_slot{}".format(i+1))
             # wandb.run.summary.update(sloti_test_acc)
-            #wandb.run.summary.update(sloti_test_f1)
-
-
-    for metrics in [f1s]:
-        df = pd.DataFrame(metrics)
-        df = df[[c for c in df.columns if "avg" not in c]]
-        saps_compactness = prepend_prefix(compute_SAP(df), "SAP_Compactness_")
-        #wandb.run.summary.update(saps_compactness)
-        avg_sap_compactness = np.mean(list(saps_compactness.values()))
-        wandb.run.summary.update({"avg_sap_compactness": avg_sap_compactness})
-
-        saps_modularity = prepend_prefix(compute_SAP(df.T), "SAP_Modularity_")
-        avg_sap_modularity = np.mean(list(saps_modularity.values()))
-        #wandb.run.summary.update(saps_modularity)
-        wandb.run.summary.update({"avg_sap_modularity": avg_sap_modularity})
+            wandb.run.summary.update(sloti_test_f1)
 
 
 
-        maxes = prepend_prefix(dict(df.max()),"best_slot_")
-        avg_of_bests = np.mean(list(maxes.values()))
-        wandb.run.summary.update({"avg_of_best_slots": avg_of_bests})
-        # argmaxes = prepend_prefix(dict(df.idxmax()), "slot_index_for_best_")
-        # wandb.run.summary.update(maxes)
-        # wandb.run.summary.update(argmaxes)
+    df, acc_df = pd.DataFrame(f1s), pd.DataFrame(accs)
+    # df = df[[c for c in df.columns if "avg" not in c]]
+    # acc_df = acc_df[[c for c in acc_df.columns if "avg" not in c]]
+    saps_compactness = append_suffix(compute_SAP(df), "_f1_sap_compactness")
+    wandb.run.summary.update(saps_compactness)
+    avg_sap_compactness = np.mean(list(saps_compactness.values()))
+    wandb.run.summary.update({"f1_avg_sap_compactness": avg_sap_compactness})
+
+    # saps_modularity = prepend_prefix(compute_SAP(df.T), "sap_modularity")
+    # avg_sap_modularity = np.mean(list(saps_modularity.values()))
+    # #wandb.run.summary.update(saps_modularity)
+    # wandb.run.summary.update({"avg_sap_modularity": avg_sap_modularity})
+
+
+
+    f1_maxes = dict(df.max())
+    acc_maxes = dict(acc_df.max())
+    acc_maxes, f1_maxes = postprocess_raw_metrics(acc_maxes, f1_maxes)
+    f1_maxes = {k:v for k,v in f1_maxes.items() if "avg" in k}
+    f1_maxes = append_suffix(f1_maxes, "_best_slot_for_each")
+    wandb.run.summary.update(f1_maxes)
+    # argmaxes = prepend_prefix(dict(df.idxmax()), "slot_index_for_best_")
+    # wandb.run.summary.update(maxes)
+    # wandb.run.summary.update(argmaxes)
 
 
 
@@ -136,6 +141,12 @@ def prepend_prefix(dictionary, prefix):
     new_dict = {}
     for k, v in dictionary.items():
         new_dict[prefix + k] = v
+    return new_dict
+
+def append_suffix(dictionary, suffix):
+    new_dict = {}
+    for k, v in dictionary.items():
+        new_dict[k + suffix] = v
     return new_dict
 
 
