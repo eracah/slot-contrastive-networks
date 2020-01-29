5c8efca13b7b94686abf4b93e5591aecab90138e
eracah <ejracah@gmail.com>
2020-01-29 02:07:45 -0500

    more launch script stuff

--- launch_scripts/unkillable_run_atariari.py
+++ launch_scripts/unkillable_run_atariari.py
@@ -2,53 +2,44 @@ import subprocess
 import sys
 base_cmd = "sbatch"
 ss= "launch_scripts/unkillable_mila_cluster.sl"
-module = "scripts.run_probe"
-args = [base_cmd, ss, module]
+# module = "scripts.run_probe"
+args = [base_cmd, ss]
 args.extend(sys.argv[1:])
 
-
-#envs = ["pong", "freeway","ms_pacman"]
-#envs = [ 'video_pinball',
- #'yars_revenge', 'space_invaders', 'private_eye', 'montezuma_revenge',
- #'ms_pacman', 'demon_attack']
-
-#envs = ['asteroids',
-#'berzerk',
-#'bowling',
-##'boxing',
-#'breakout',
-#'demon_attack',
-#'freeway',
-#'frostbite',
-#'hero',
-#'montezumai_revenge',
-#'ms_pacman',
-#'pitfall',
-#'pong',
-#'private_eye',
-#'qbert',
-#'riverraid',
-#'seaquest',
-#'space_invaders',
-#'tennis',
-#'venture',
-#'video_pinball',
-#'yars_revenge']
-# envs =  ["asteroids", "freeway", "montezuma_revenge", 'berzerk', 'boxing',
-#         'demon_attack', 'enduro', 'freeway', 'frostbite', 'hero',
-#         'ms_pacman', 'pong', 'private_eye', 'qbert', 'riverraid',
-#         'seaquest', 'solaris', 'space_invaders', 'venture', 'video_pinball',
-#         'yars_revenge','breakout','pitfall','montezuma_revenge'
-#         ]
+envs = ['asteroids',
+'berzerk',
+'bowling',
+'boxing',
+'breakout',
+'demon_attack',
+'freeway',
+'frostbite',
+'hero',
+'montezumai_revenge',
+'ms_pacman',
+'pitfall',
+'pong',
+'private_eye',
+'qbert',
+'riverraid',
+'seaquest',
+'space_invaders',
+'tennis',
+'venture',
+'video_pinball',
+'yars_revenge']
 
 
-#suffix = "NoFrameskip-v4"
-#names = env.split("_")
-#name = "".join([s.capitalize() for s in names])
- #   sargs = args + ["--env-name"]
+suffix = "NoFrameskip-v4"
+for i,env in enumerate(envs):
 
-#  sargs.append(name + suffix)
+    names = env.split("_")
+    name = "".join([s.capitalize() for s in names])
+    sargs = args + ["--env-name"]
 
-args.extend(["--wandb-proj", "coors-production"])
-print(" ".join(args))
-subprocess.run(args)
+    sargs.append(name + suffix)
+
+    sargs.extend(["--wandb-proj", "coors-production"])
+
+    print(" ".join(sargs))
+    subprocess.run(sargs)
