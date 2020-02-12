be8fd79f4f304c59f874b1d786d17cfbcfd4e9e9
eracah <ejracah@gmail.com>
2020-02-12 06:06:26 -0500

    vectorized stdim

--- src/baselines/stdim.py
+++ src/baselines/stdim.py
@@ -12,47 +12,35 @@ class STDIMModel(nn.Module):
         self.emnedding_dim = embedding_dim
         self.wandb = wandb
 
-        self.classifier1 = nn.Linear(self.emnedding_dim, self.fmap_encoder.local_layer_depth).to(device)  # x1 = global, x2=patch, n_channels = 32
-        self.classifier2 = nn.Linear(self.fmap_encoder.local_layer_depth, self.fmap_encoder.local_layer_depth).to(device)
+        self.score_fxn1 = nn.Linear(self.emnedding_dim, self.fmap_encoder.local_layer_depth).to(device)  # x1 = global, x2=patch, n_channels = 32
+        self.score_fxn2 = nn.Linear(self.fmap_encoder.local_layer_depth, self.fmap_encoder.local_layer_depth).to(device)
         self.device = device
 
     def calc_loss(self, xt, a, xtp1):
         f_t = self.encoder(xt)
         fmap_t = self.fmap_encoder(xt).permute(0, 2, 3, 1)
         fmap_tp1 = self.fmap_encoder(xtp1).permute(0, 2, 3, 1)
+        N, sy, sx, d = fmap_tp1.shape
+
         # Loss 1: Global at time t, f5 patches at time t-1
-        sy = fmap_tp1.size(1)
-        sx = fmap_tp1.size(2)
-        N = f_t.size(0)
-        loss1 = 0.
-        acc1 = 0.
-        for y in range(sy):
-            for x in range(sx):
-                predictions = self.classifier1(f_t)
-                positive = fmap_tp1[:, y, x, :]
-                logits = torch.matmul(predictions, positive.t())
-                target = torch.arange(N).to(self.device)
-                step_loss = F.cross_entropy(logits, target)
-                acc1 += calculate_accuracy(logits.detach().cpu().numpy(), target.detach().cpu().numpy())
-                loss1 += step_loss
-        loss1 = loss1 / (sx * sy)
-        acc1 = acc1 / (sx * sy)
+        glob_score = self.score_fxn1(f_t)
+        local_flattened = fmap_tp1.reshape(-1, d)
+        logits1 = torch.matmul(local_flattened, glob_score.t()).reshape(N, sy * sx, -1).transpose(1, 0).reshape(-1, N)
+        target1 = torch.arange(N).repeat(sx * sy)
+        loss1 = nn.CrossEntropyLoss()(logits1, target1)
+
 
         # Loss 2: f5 patches at time t, with f5 patches at time t-1
-        loss2 = 0.
-        acc2 = 0.
-        for y in range(sy):
-            for x in range(sx):
-                predictions = self.classifier2(fmap_t[:, y, x, :])
-                positive = fmap_tp1[:, y, x, :]
-                logits = torch.matmul(predictions, positive.t())
-                target = torch.arange(N).to(self.device)
-                step_loss = F.cross_entropy(logits, target)
-                acc2 += calculate_accuracy(logits.detach().cpu().numpy(), target.detach().cpu().numpy())
-                loss2 += step_loss
-        loss2 = loss2 / (sx * sy)
-        acc2 = acc2 / (sx * sy)
+        local_t_score = self.score_fxn2(fmap_t.reshape(-1, d))
+        transformed_local_t = local_t_score.reshape(N, sy*sx,d).transpose(0,1)
+        local_tp1 = fmap_tp1.reshape(N, sy * sx, d).transpose(0, 1)
+        logits2 = torch.matmul(transformed_local_t, local_tp1.transpose(1, 2)).reshape(-1, N)
+        target2 = torch.arange(N).repeat(sx * sy)
+        loss2 = nn.CrossEntropyLoss()(logits2, target2)
+
         loss = loss1 + loss2
+        acc1 = calculate_accuracy(logits1.detach().cpu().numpy(), target1.detach().cpu().numpy())
+        acc2 = calculate_accuracy(logits2.detach().cpu().numpy(), target2.detach().cpu().numpy())
         if self.training:
             self.wandb.log({"tr_acc1": acc1})
             self.wandb.log({"tr_acc2": acc2})
