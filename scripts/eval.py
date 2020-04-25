65cf5f1f430d032e9ada13c8e496445800d1dbaa
eracah <ejracah@gmail.com>
2020-04-24 17:31:37 -0700

    add case where can't access wandb servers and we use directory on filesystem instead

--- scripts/eval.py
+++ scripts/eval.py
@@ -12,21 +12,21 @@ from scripts.train import get_encoder
 from src.data.stdim_dataloader import get_stdim_dataloader
 from src.data.cswm_dataloader import get_cswm_dataloader
 import copy
+from pathlib import Path
+
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # eval
     parser = argparse.ArgumentParser()
     parser.add_argument("--run-dir", type=str, default=".")
+    parser.add_argument("--tr-dir", type=str, default=".")
     parser.add_argument('--num-frames', type=int, default=50000, help='Number of steps to train probes (default: 30000 )')
     parser.add_argument('--num-episodes', type=int, default=100)
     parser.add_argument("--collect-mode", type=str, choices=["random_agent", "pretrained_ppo", "cswm"],default="random_agent", help="how we collect the data")
-    parser.add_argument('--num-processes', type=int, default=8,help='Number of parallel environments to collect samples from (default: 8)')
     parser.add_argument('--seed', type=int, default=11, help='Random seed to use')
-    parser.add_argument("--patience", type=int, default=15)
     parser.add_argument('--lr', type=float, default=3e-4, help='Learning Rate for learning representations (default: 5e-4)')
     parser.add_argument('--batch-size', type=int, default=64, help='Mini-Batch Size (default: 64)')
-    parser.add_argument('--epochs', type=int, default=300, help='Number of epochs for  (default: 100)')
     parser.add_argument("--wandb-proj", type=str, default="coors-scratch")
     parser.add_argument("--id", type=str)
     parser.add_argument("--entropy-threshold", type=float, default=0.6)
@@ -35,20 +35,33 @@ if __name__ == "__main__":
 
     wandb.init(project=args.wandb_proj, dir=args.run_dir, tags=["eval"])
 
-    # list_of_files = [f for f in glob.glob(args.train_run_parent_dir + "/*") if os.path.isdir(f) and "run" in os.path.basename(f)]  # * means all if need specific format then *.csv
-    # latest_file = max(list_of_files, key=os.path.getctime)
-    # args.train_run_dirname = Path(latest_file).name
-    api = wandb.Api()
-    path = "/".join(["eracah", args.wandb_proj, args.id])
-    print(path)
-    run = api.run(path=path)
-    json_obj = run.file(name="wandb-metadata.json").download(root=wandb.run.dir + "/train_run", replace=True)
-    train_args = json.load(open(json_obj.name))["args"]
-    file_obj = run.file(name="encoder.pt").download(root=wandb.run.dir, replace=True)
-    weights_path = file_obj.name
 
-    train_parser = get_train_argparser()
-    train_args = run.config
+    try:
+        api = wandb.Api()
+        path = "/".join(["eracah", args.wandb_proj, args.id])
+        print(path)
+        run = api.run(path=path)
+        json_obj = run.file(name="wandb-metadata.json").download(root=wandb.run.dir + "/train_run", replace=True)
+        train_args = json.load(open(json_obj.name))["args"]
+        file_obj = run.file(name="encoder.pt").download(root=wandb.run.dir, replace=True)
+        weights_path = file_obj.name
+        train_args = run.config
+    except:
+        path = Path(args.tr_dir)
+        print(path)
+        json_obj = path / Path("wandb-metadata.json")
+        train_args = json.load(open(str(json_obj)))["args"]
+        weights_path = path / Path("encoder.pt")
+        train_parser = get_train_argparser()
+        train_args = train_parser.parse_args(train_args)
+        train_args = train_args.__dict__
+
+
+
+
+
+
+
     for k, v in train_args.items():
         if k in args:
             args.__dict__["train_" + k] = v
